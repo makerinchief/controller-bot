2198d072abd62156181b4096f522dd78b36832cd
Makerinchief <info@makerinchief.com>
2023-12-28 14:54:14 -0500

    cleans debt

--- Controller_Bot.ino
+++ Controller_Bot.ino
@@ -9,18 +9,10 @@
   Please read README.md for details
 */
 
-//NRFLite is used to make communication easy
-//https://github.com/dparson55/NRFLite
+
+// Basic script to print joystick values to Serial Monitor
+
 #include <SPI.h>
-// #include <NRFLite.h>
-// #include "datatypes.h"
-
-// NRFLite controller_radio;
-
-// typedef struct TestStruct {
-//   int val_1;
-//   int val_2;
-// };
 
 typedef struct JoyStickValues {
   int16_t left_x;
@@ -31,27 +23,7 @@ typedef struct JoyStickValues {
   int16_t right_b;
 };
 
-typedef struct ControllerPacket {
-  int16_t left_x;
-  int16_t left_y;
-  int16_t left_b;
-  int16_t right_x;
-  int16_t right_y;
-  int16_t right_b;
-};
-
-// typedef struct TankBotPacket {
-//   char direction;
-//   int16_t left_speed;
-//   int16_t right_speed;
-// };
-
 JoyStickValues joystick_values;
-ControllerPacket controller_packet;
-// TankBotPacket tank_packet;
-
-//The joystick data can be noisy so we can create a threshold for each axis
-// int16_t axis_threshold = 25;
 
 //Analog Pins 0 - 3
 const int LY_PIN = 14;
@@ -63,14 +35,9 @@ const int RX_PIN = 17;
 const int LB_PIN = 3;
 const int RB_PIN = 4;
 
-//Use a timer instead of delay() in order to multitask
-unsigned long joystick_current = 0;
-unsigned long joystick_previous = 0;
-long joystick_interval = 10;
-
 unsigned long controller_current = 0;
 unsigned long controller_previous = 0;
-long controller_interval = 100;
+long controller_interval = 250;
 
 void setup() {
 
@@ -79,17 +46,6 @@ void setup() {
   pinMode(RB_PIN, INPUT_PULLUP);
   pinMode(LB_PIN, INPUT_PULLUP);
 
-  // joysticks_init();
-  // display_init();
-  // display_test();
-  // delay(2000);
-  // display_clear();
-  // display_text(0, 1, "Controller Bot");
-  // if (!radio_init()) {
-  //   Serial.println("!NO RADIO FOUND!");
-  //   display_text(1, 0, "NO RADIO FOUND");
-  // }
-  
 }
 
 void loop() {
@@ -119,48 +75,6 @@ void loop() {
     Serial.print(joystick_values.right_y);
     Serial.print(" : ");
     Serial.println(joystick_values.right_b);
-
-    //   joystick_vals = joysticks_read();
-    //   //  joysticks_print_values();
-    //   //  display_joysticks(joystick_vals);
-    //   int l_spd = map(joystick_vals.left_y, 0, 1023, 256, -256);
-    //   int r_spd = map(joystick_vals.right_y, 0, 1023, 256, -256);
-    //   char direction;
-    //   if (abs(l_spd) > axis_threshold) {
-    //     tank_packet.left_speed = l_spd;
-    //   } else {
-    //     tank_packet.left_speed = 0;
-    //   }
-    //   if (abs(r_spd) > axis_threshold) {
-    //     tank_packet.right_speed = r_spd;
-    //   } else {
-    //     tank_packet.right_speed = 0;
-    //   }
-    //   if (l_spd > 0 && r_spd > 0) {
-    //     direction = 'f';
-    //   }
-    //   if (l_spd < 0 && r_spd < 0) {
-    //     direction = 'b';
-    //   }
-    //   if (l_spd < 0 && r_spd > 0) {
-    //     direction = 'l';
-    //   }
-    //   if (l_spd > 0 && r_spd < 0) {
-    //     direction = 'r';
-    //   }
-    //   if (abs(l_spd) <= axis_threshold &&
-    //       abs(r_spd) <= axis_threshold) {
-    //     direction = 's';
-    //   }
-    //   tank_packet.direction = direction;
-    //   Serial.print("L SPD: ");
-    //   Serial.print(abs(l_spd));
-    //   Serial.print(" R SPD: ");
-    //   Serial.print(abs(r_spd));
-    //   Serial.print(" DIR: ");
-    //   Serial.println(direction);
-    //   radio_send_tankbot(tank_packet);
-    //   controller_previous = controller_previous;
 
   }
 }
